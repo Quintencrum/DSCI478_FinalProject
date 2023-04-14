e7cd9e6d2ae0ef336cae8b09615b618e46f5c7ea
Quintencrum <quintencrum@gmail.com>
2023-04-13 18:42:12 -0600

    saving

--- dataImport.py
+++ dataImport.py
@@ -7,23 +7,21 @@ import tensorflow as tf
 
 
 def data_Import():
-    stock = [[]]
-
     ibb_full = pd.read_csv('data/Deep_Portfolio/ibb.csv', index_col=0).astype('float32')
 
     stock_lp = pd.read_csv('data/Deep_Portfolio/last_price.csv', index_col=0).dropna(axis=1, how='any').astype('float32')
-    # stock['calibrate']['lp'] = stock_lp.iloc[0:104, :]
-    # stock['validate']['lp'] = stock_lp.iloc[104:, :]
+    stock_calibrate_lp = stock_lp.iloc[0:104, :]
+    stock_validate_lp = stock_lp.iloc[104:, :]
 
     stock_net = pd.read_csv('data/Deep_Portfolio/net_change.csv', index_col=0).dropna(axis=1, how='any').astype('float32')
-    # stock['calibrate']['net'] = stock_net.iloc[0:104, :]
-    # stock['validate']['net'] = stock_net.iloc[104:, :]
+    stock_calibrate_net = stock_net.iloc[0:104, :]
+    stock_validate_net = stock_net.iloc[104:, :]
 
     stock_percentage = pd.read_csv('data/Deep_Portfolio/percentage_change.csv', index_col=0).dropna(axis=1, how='any').astype('float32')
-    # stock['calibrate']['percentage'] = stock_percentage.iloc[0:104, :]
-    # stock['validate']['percentage'] = stock_percentage.iloc[104:, :]
+    stock_calibrate_percentage = stock_percentage.iloc[0:104, :]
+    stock_validate_percentage = stock_percentage.iloc[104:, :]
 
-    return ibb_full, stock, stock_lp, stock_net, stock_percentage
+    return ibb_full, stock_lp, stock_calibrate_lp, stock_validate_lp, stock_net, stock_calibrate_net, stock_validate_net, stock_percentage, stock_calibrate_percentage, stock_validate_percentage
 
 
-data_Import()
+print(data_Import())
