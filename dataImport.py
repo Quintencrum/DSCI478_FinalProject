3577aefa463e103eff484bbdbd2dcb8b07effe34
Quintencrum <quintencrum@gmail.com>
2023-04-13 18:48:44 -0600

    saving

--- dataImport.py
+++ dataImport.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 
 
-def data_Import():
+def data_Import_ibb():
     ibb_full = pd.read_csv('data/Deep_Portfolio/ibb.csv', index_col=0).astype('float32')
 
     stock_lp = pd.read_csv('data/Deep_Portfolio/last_price.csv', index_col=0).dropna(axis=1, how='any').astype('float32')
@@ -24,4 +24,8 @@ def data_Import():
     return ibb_full, stock_lp, stock_calibrate_lp, stock_validate_lp, stock_net, stock_calibrate_net, stock_validate_net, stock_percentage, stock_calibrate_percentage, stock_validate_percentage
 
 
-print(data_Import())
+def data_import_ETFs():
+    
+
+# def data_import_spx():
+#     pd.read_csv('data/PCA_Factors/spx_holdings_and_spx_closeprice.csv')
