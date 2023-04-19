e29648d09e9b348a81c8d03070127d033afe9962
Jorie Alvis <jorie@mcpsych.com>
2023-04-19 14:16:32 -0600

    Data processing

--- src/utilities.py
+++ src/utilities.py
@@ -5,4 +5,4 @@ def get_project_path():
     return Path.cwd()
 
 def get_data_path():
-    data_path = get_project_path().parent.joinpath("data")
+    return get_project_path().parent.joinpath("data")
