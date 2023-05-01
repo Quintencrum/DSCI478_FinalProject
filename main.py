806f05b7eb02d17018e2e8db98824eee3357bc40
Quintencrum <quintencrum@gmail.com>
2023-05-01 10:14:05 -0600

    saving

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 
 from data import dataImport, visualize
 from models import networkGraphs_dataPrep, networkGraphs_plot
+import numpy as np
 
 
 # def main(parsed):
@@ -23,14 +24,26 @@ def main():
     print('Hello World!')
     allStock_df_list = networkGraphs_dataPrep.networkDataPrep(dataImport.data_import_ng())
 
+    close_dist_matrix = networkGraphs_dataPrep.df_distance_correlation_2(allStock_df_list[0])
+
+    print(close_dist_matrix)
+
+    networkGraphs_plot.plot_network(close_dist_matrix,'Hello')
+
+
+
+
+
+
     # df_dcor_list = [networkGraphs_dataPrep.distance_correlation_matrix(df) for df in allStock_df_list]
 
-    df_dcor_list = [networkGraphs_dataPrep.df_distance_correlation(df) for df in allStock_df_list]
+    # df_dcor_list = [networkGraphs_dataPrep.df_distance_correlation(df) for df in allStock_df_list]
 
-    print(df_dcor_list[4].head())
+    # print(df_dcor_list[4].head())
 
-    # dist_corr_matrix = networkGraphs_dataPrep.distance_correlation_matrix(allStock_df)
 
+    # dist_corr_matrix = networkGraphs_dataPrep.distance_correlation_matrix(allStock_df_list[0])
+    # print(allStock_df_list[0])
     # print(dist_corr_matrix)
 
     # networkGraphs_plot.plot_network(df_dcor_list[4],'Hello')
--- models/networkGraphs_dataPrep.py
+++ models/networkGraphs_dataPrep.py
@@ -124,45 +124,66 @@ def distCorr(x, y):
         return 0.0
     else:
         return np.sqrt(dcovXY / np.sqrt(dvarX * dvarY))
-
-
-
-
-
-def distance_correlation_matrix(df):
-    df_dcor = pd.DataFrame(index=df.columns, columns=df.columns)
     
-    # Pairwise Euclidean distance matrix
-    dist_mat = np.sqrt((df ** 2).sum(axis=0).values.reshape(-1,1) + 
-                       (df ** 2).sum(axis=0).values - 2 * df.T.dot(df))
+
+def distCorr_optimized(x, y):
+    n = len(x)
+    a = np.abs(x[:, np.newaxis] - x)
+    b = np.abs(y[:, np.newaxis] - y)
+    C = np.sum(a)
+    D = np.sum(b)
+    E = np.sum(a**2)
+    F = np.sum(b**2)
+
+    covXY = np.sum(a * b)
+
+    dcovXY = covXY - C * D / n
+    dvarX = (E - C ** 2 / n) / n
+    dvarY = (F - D ** 2 / n) / n
+
+    if dvarX * dvarY == 0:
+        return 0.0
+    else:
+        return np.sqrt(dcovXY / np.sqrt(dvarX * dvarY))
+
+
+
+
+
+# def distance_correlation_matrix(df):
+#     df_dcor = pd.DataFrame(index=df.columns, columns=df.columns)
     
-    #                   = distance mat - row mean - column mean + total mean
-    # dist_mat_centered = dist_mat - dist_mat.mean(axis=1).reshape(-1,1) - dist_mat.mean(axis=0) + dist_mat.mean()
-    row_mean = dist_mat.mean(axis=1).values
-    column_mean = dist_mat.mean(axis=0)
-    dist_mat_centered = dist_mat - row_mean.reshape(-1,1) - column_mean + dist_mat.mean()
+#     # Pairwise Euclidean distance matrix
+#     dist_mat = np.sqrt((df ** 2).sum(axis=0).values.reshape(-1,1) + 
+#                        (df ** 2).sum(axis=0).values - 2 * df.T.dot(df))
+    
+#     #                   = distance mat - row mean - column mean + total mean
+#     # dist_mat_centered = dist_mat - dist_mat.mean(axis=1).reshape(-1,1) - dist_mat.mean(axis=0) + dist_mat.mean()
+#     row_mean = dist_mat.mean(axis=1).values
+#     column_mean = dist_mat.mean(axis=0)
+#     dist_mat_centered = dist_mat - row_mean.reshape(-1,1) - column_mean + dist_mat.mean()
 
     
-    # hopefully compute the sample covariance and variances of the row and column vectors
-    cov_mat = np.cov(dist_mat_centered, rowvar=False)
-    row_vars = np.var(dist_mat_centered, axis=1, ddof=1)
-    col_vars = np.var(dist_mat_centered, axis=0, ddof=1)
+#     # hopefully compute the sample covariance and variances of the row and column vectors
+#     cov_mat = np.cov(dist_mat_centered, rowvar=False)
+#     row_vars = np.var(dist_mat_centered, axis=1, ddof=1)
+#     col_vars = np.var(dist_mat_centered, axis=0, ddof=1)
     
-    # computes distance correlation coefficients
-    for i, stock_i in enumerate(df.columns):
-        for j, stock_j in enumerate(df.columns):
-            numerator = cov_mat[i, j]
-            denominator = np.sqrt(row_vars[i] * col_vars[j])
-            if denominator == 0:
-                dcor_val = 0
-            else:
-                dcor_val = numerator / denominator
+#     # computes distance correlation coefficients
+#     for i, stock_i in enumerate(df.columns):
+#         for j, stock_j in enumerate(df.columns):
+#             numerator = cov_mat[i, j]
+#             denominator = np.sqrt(row_vars[i] * col_vars[j])
+#             if denominator == 0:
+#                 dcor_val = 0
+#             else:
+#                 dcor_val = numerator / denominator
 
-            df_dcor.at[stock_i, stock_j] = dcor_val
+#             df_dcor.at[stock_i, stock_j] = dcor_val
 
-    np.fill_diagonal(df_dcor.values, 1)
+#     np.fill_diagonal(df_dcor.values, 1)
     
-    return df_dcor
+#     return df_dcor
 
 
 def df_distance_correlation(df_train):
@@ -176,7 +197,8 @@ def df_distance_correlation(df_train):
             print(j)
             stock_i = df_train.iloc[:, i].values
             stock_j = df_train.iloc[:, j].values
-            distcorr_ij = distCorr(stock_i, stock_j)
+            # distcorr_ij = distCorr(stock_i, stock_j)
+            distcorr_ij = distCorr_optimized(stock_i, stock_j)
             distcorr_matrix[i, j] = distcorr_ij
             distcorr_matrix[j, i] = distcorr_ij
 
@@ -185,3 +207,131 @@ def df_distance_correlation(df_train):
 
 
 
+
+###########
+
+# Helper function to calculate covariance between two vectors
+def cov(x, y):
+    n = len(x)
+    return np.dot(x - x.mean(), y - y.mean()) / (n - 1)
+
+# Helper function to calculate distance correlation between two vectors
+def distance_correlation(x, y):
+    print('start: ')
+    # print(x)
+    # print(y)
+
+    if np.isnan(x.any()):
+        print('X has NAN values')
+    if np.isnan(y.any()):
+        print('Y has NAN values')
+
+    n = len(x)
+    cov_xy = cov(x, y)
+    var_x = cov(x, x)
+    var_y = cov(y, y)
+
+    # print(var_x)
+    # print(var_y)
+
+
+    a = np.sqrt(var_x * var_y)
+    b = np.sum(np.outer(x, x), axis=1)[:, None] - 2 * np.dot(x.reshape(-1, 1), y.reshape(1, -1)) + np.sum(np.outer(y, y), axis=1)[None, :]
+    b = np.sqrt(b)
+
+    if(np.isnan(b).any()):
+        print('B is nan: 1')
+        if(np.isnan(np.sum(np.outer(x, x), axis=1)[:, None]).any()):
+            print('B is nan: 2')
+        # else:
+        #     print(np.sum(np.outer(x, x), axis=1)[:, None].shape)
+        if(np.isnan(2 * np.dot(x.reshape(-1, 1), y.reshape(1, -1))).any()):
+           print('B is nan: 3') 
+        # else:
+        #     print(2 * np.dot(x.reshape(-1, 1), y.reshape(1, -1)).shape)
+        if(np.isnan(np.sum(np.outer(y, y), axis=1)[None, :]).any()):
+            print('B is nan: 4') 
+        # else:
+        #     print(np.sum(np.outer(y, y), axis=1)[None, :].shape)
+        if(np.isnan(np.sum(np.outer(x, x), axis=1)[:, None] - 2 * np.dot(x.reshape(-1, 1), y.reshape(1, -1)) + np.sum(np.outer(y, y), axis=1)[None, :]).any()):
+            print('B is nan: 5') 
+        else:
+            # print(np.sum(np.outer(x, x), axis=1)[:, None] - 2 * np.dot(x.reshape(-1, 1), y.reshape(1, -1)) + np.sum(np.outer(y, y), axis=1)[None, :])
+            print(np.sqrt(np.sum(np.outer(x, x), axis=1)[:, None] - 2 * np.dot(x.reshape(-1, 1), y.reshape(1, -1)) + np.sum(np.outer(y, y), axis=1)[None, :]))
+
+
+    # print('a: ' + str(a))
+    # print('b: ' + str(b))
+
+    dcov_xy = np.sum(b) / (n * (n - 1))
+    
+    # print('end:-------------')
+    if a == 0:
+        return 0
+    else:
+        return dcov_xy / a
+
+
+def distcorr(x, y):
+    import numpy as np
+    import pandas as pd
+    from sklearn.metrics.pairwise import pairwise_distances
+    import tensorflow as tf
+    import matplotlib.pyplot as plt
+
+    # generate two arrays of random data
+    x = np.random.rand(100)
+    y = np.random.rand(100)
+
+    # compute the pairwise distance matrices for x and y
+    Dx = pairwise_distances(x.reshape(-1,1))
+    Dy = pairwise_distances(y.reshape(-1,1))
+
+    # compute the mean-centered distance matrices for x and y
+    Hx = (Dx - np.mean(Dx, axis=0) - np.mean(Dx, axis=1) + np.mean(Dx))
+    Hy = (Dy - np.mean(Dy, axis=0) - np.mean(Dy, axis=1) + np.mean(Dy))
+
+    # compute the distance covariance and distance standard deviations
+    dcov = np.sqrt(np.sum(Hx * Hy) / (len(x)**2))
+    dvarx = np.sqrt(np.sum(Hx**2) / (len(x)**2))
+    dvary = np.sqrt(np.sum(Hy**2) / (len(x)**2))
+
+    # compute the distance correlation
+    dcorr = dcov / np.sqrt(dvarx * dvary)
+
+    # print the result
+    # print(dcorr)
+
+    # plot the data
+    # plt.scatter(x, y)
+    # plt.show()
+    if(np.isnan(dcorr).any()):
+        # print('NAN found')
+        # print(dcorr)
+        return np.nan_to_num(dcorr)
+
+    return dcorr
+
+
+
+
+# Function to compute the distance correlation (dcor) matrix from a DataFrame and output a DataFrame of dcor values.
+def df_distance_correlation_2(df_train):
+    stocks = df_train.columns
+    n_stocks = len(stocks)
+    df_train_dcor = pd.DataFrame(index=stocks, columns=stocks)
+    # print(n_stocks)
+    for i in range(n_stocks):
+        # print(i)
+        v_i = df_train.iloc[:, i].values
+        for j in range(i, n_stocks):
+            # print(j)
+            v_j = df_train.iloc[:, j].values
+            # dcor_val = distance_correlation(v_i, v_j)
+            dcor_val = distcorr(v_i, v_j)
+
+            # print('dcor_val: '+ str(dcor_val))
+            # print('v_i, v_j:' + str(v_i) + ', ' + str(v_j))
+            df_train_dcor.at[stocks[i], stocks[j]] = dcor_val
+            df_train_dcor.at[stocks[j], stocks[i]] = dcor_val
+    return df_train_dcor
