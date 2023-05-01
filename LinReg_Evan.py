f6ac5d26889465d4707e7c27bc1e89f8a722ecef
Evan Fiechtner <103147970+evan-fiechtner@users.noreply.github.com>
2023-05-01 15:19:06 -0600

    Update LinReg_Evan.py
    Updated Code and added labels for the plots

--- LinReg_Evan.py
+++ LinReg_Evan.py
@@ -2,102 +2,102 @@
 import numpy as np
 import pandas as pd
 import matplotlib as plt
-
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 
-# Loading data frames from Github -- df = Our df
+# Loading data frames from Github -- df = Our df, returns = source df, ibb = index df
 df = pd.read_csv('https://raw.githubusercontent.com/Quintencrum/DSCI478_FinalProject/Q/data/Network_Graphs/all_stocks.csv')
 returns = pd.read_csv('https://raw.githubusercontent.com/firmai/techniques/master/Assets/returns.csv')
 ibb = pd.read_csv('https://raw.githubusercontent.com/Quintencrum/DSCI478_FinalProject/main/data/Deep_Portfolio/ibb.csv')
 
-# dataframe manipulation
+# ----------   Our Dataframe manipulation   ----------
 df['return'] = df['Open'] - df['Close']
 df1 = df.drop(['Open', 'High', 'Low', 'Close','Volume'], axis=1)
 
-df1['Name'].unique()
+df1['Name'].unique() # returns names of all stocks used
 df2 = df1.pivot(index='Date', columns='Name', values='return')
 df2 = df2.fillna(0)
 df2['label'] = 1
 
 df2 = df2.reset_index()
-df2 = df2.drop(['Date'], axis=1) # dates starting in 2006-01-03, going till 2017-12-29
+df2 = df2.drop(['Date'], axis=1) # dates starting in 2006-01-03, going till 2017-12-29, represented as 0,1,...,n.
 
-## Beginning of Regression Section
+
+## ----------   Beginning of Regression Section   ----------
+
 # Normal Linear Regression Weights
-reg = LinearRegression(fit_intercept=False).fit(df2.drop(["label"],axis=1), df2["label"])
+normal_linreg_df = LinearRegression(fit_intercept=False).fit(df2.drop(["label"],axis=1), df2["label"])
 
 coeffs = pd.DataFrame(df2.drop(['label'], axis=1).columns)
 coeffs.columns = ["Tickers"]
-coeffs["Coefficients"] = reg.coef_
+coeffs["Coefficients"] = normal_linreg_df.coef_
 coeffs["Weights"] = (coeffs["Coefficients"].abs() / coeffs["Coefficients"].abs().sum())*100
-coeffs.sort_values('Weights', ascending=False).head(7)
+coeffs.sort_values('Weights', ascending=False).head(5) # real weights for all 30 stocks
 
 
 # Lasso Regression Weights
 
-reg = Lasso(fit_intercept=False,alpha=0.00005, positive=True).fit(df2.drop(["label"],axis=1), df2["label"])
+lasso_linreg_df = Lasso(fit_intercept=False,alpha=0.00005, positive=True).fit(df2.drop(["label"],axis=1), df2["label"])
 
 lasso_coeffs = pd.DataFrame(df2.drop(["label"],axis=1).columns)
 lasso_coeffs.columns = ["Tickers"]
-lasso_coeffs["Coefficients"] = reg.coef_
+lasso_coeffs["Coefficients"] = lasso_linreg_df.coef_
 lasso_coeffs["Weights"] = (lasso_coeffs["Coefficients"].abs() / lasso_coeffs["Coefficients"].abs().sum())*100
 
-lasso_coeffs.sort_values('Weights', ascending=False).head(7) # reduces ALL others to zero
+lasso_coeffs.sort_values('Weights', ascending=False).head(5) # reduces ALL others to zero
 
 # Ridge Regression Weights
 
-reg = Ridge(fit_intercept=False,alpha=1, positive=True).fit(df2.drop(["label"],axis=1), df2["label"])
+ridge_reg_df = Ridge(fit_intercept=False,alpha=1, positive=True).fit(df2.drop(["label"],axis=1), df2["label"])
 
 ridge_coeffs = pd.DataFrame(df2.drop(["label"],axis=1).columns)
 ridge_coeffs.columns = ["Tickers"]
-ridge_coeffs["Coefficients"] = reg.coef_
+ridge_coeffs["Coefficients"] = ridge_reg_df.coef_
 ridge_coeffs["Weights"] = (ridge_coeffs["Coefficients"].abs() / ridge_coeffs["Coefficients"].abs().sum())*100
 
-ridge_coeffs.sort_values('Weights', ascending=False).head(7) # reduces ALL others to zero
+ridge_coeffs.sort_values('Weights', ascending=False).head(5) # reduces ALL others to zero
 
-## End of Regression for Our Dataframe
+## ----------   End of Regression for Our Dataframe   ----------
 
-## Beginning Regression on Their Dataframe ('return' dataframe)
+## ----------   Beginning Regression on Their Dataframe ('return' dataframe)   ----------
 
 returns = returns.set_index('date', drop=True) # set index correctly
-
 returns["label"] = 1 
 
 # Normal Regression Section
-reg = LinearRegression(fit_intercept=False).fit(returns.drop(["label"],axis=1), returns["label"])
+normal_linreg_source = LinearRegression(fit_intercept=False).fit(returns.drop(["label"],axis=1), returns["label"])
 
 normal_coeffs = pd.DataFrame(returns.drop(["label"],axis=1).columns)
-normal_coeffs.columns = ["TICKER"]
-normal_coeffs["coeff"] = reg.coef_
-normal_coeffs["weight"] = (normal_coeffs["coeff"].abs() / normal_coeffs["coeff"].abs().sum())*100
+normal_coeffs.columns = ["Tickers"]
+normal_coeffs["Coefficients"] = normal_linreg_source.coef_
+normal_coeffs["Weights"] = (normal_coeffs["Coefficients"].abs() / normal_coeffs["Coefficients"].abs().sum())*100
 
 print("")
 print("Normal Regression Weights")
-print(normal_coeffs.sort_values('weight', ascending=False).head(5))
+print(normal_coeffs.sort_values('Weights', ascending=False).head(5))
 
 # Lasso Regression Section
-reg = Lasso(fit_intercept=False,alpha=0.0005, positive=True).fit(returns.drop(["label"],axis=1), returns["label"])
+lasso_reg_source = Lasso(fit_intercept=False,alpha=0.0005, positive=True).fit(returns.drop(["label"],axis=1), returns["label"])
 
 lasso_coeffs = pd.DataFrame(returns.drop(["label"],axis=1).columns)
-lasso_coeffs.columns = ["TICKER"]
-lasso_coeffs["coeff"] = reg.coef_
-lasso_coeffs["weight"] = (lasso_coeffs["coeff"].abs() / lasso_coeffs["coeff"].abs().sum())*100
+lasso_coeffs.columns = ["Tickers"]
+lasso_coeffs["Coefficients"] = lasso_reg_source.coef_
+lasso_coeffs["Weights"] = (lasso_coeffs["Coefficients"].abs() / lasso_coeffs["Coefficients"].abs().sum())*100
 
 
 print("Lasso Regression Weights, alpha=0.0005")
-print(lasso_coeffs.sort_values('weight', ascending=False).head(5))
+print(lasso_coeffs.sort_values('Weights', ascending=False).head(5))
 
 # Ridge Regression Section
-reg = Ridge(fit_intercept=False,alpha=1).fit(returns.drop(["label"],axis=1), returns["label"])
+ridge_reg_source = Ridge(fit_intercept=False,alpha=1).fit(returns.drop(["label"],axis=1), returns["label"])
 
 ridge_coeffs = pd.DataFrame(returns.drop(["label"],axis=1).columns)
-ridge_coeffs.columns = ["TICKER"]
-ridge_coeffs["coeff"] = reg.coef_
-ridge_coeffs["weight"] = (ridge_coeffs["coeff"].abs() / ridge_coeffs["coeff"].abs().sum())*100
+ridge_coeffs.columns = ["Tickers"]
+ridge_coeffs["Coefficients"] = ridge_reg_source.coef_
+ridge_coeffs["Weights"] = (ridge_coeffs["Coefficients"].abs() / ridge_coeffs["Coefficients"].abs().sum())*100
 
 print("")
 print("Ridge Regression Weights")
-print(ridge_coeffs.sort_values('weight', ascending=False).head(5))
+print(ridge_coeffs.sort_values('Weights', ascending=False).head(5))
 
 
 ## End of Regression for Their Dataframes
@@ -124,7 +124,7 @@ normal_returns = normal_returns[['NURO','NFLX','SCON','RBCN','HHS']]
 lasso_returns = lasso_returns[['RGCO','FCAP','DLTR','HIFS','EXPO']]
 ridge_returns = ridge_returns[['NURO','NFLX','SCON','RBCN','HHS']]
 
-# Data point reduction, 15 data points total (helps make graphs more readable)
+# Data point reduction, 15 data points total (Making graphs more readable)
 normal_df = normal_df.loc[::201] # 15 data points
 lasso_df = lasso_df.loc[::201] # 15 data points
 ridge_df = ridge_df.loc[::201] # 15 data points
@@ -157,8 +157,11 @@ print('')
 print('Total Sum Returns:  ', normal_df_returns)
 
 # Plot of Returns for Normal DF
-normal_df.plot(use_index=True, y=['GE', 'VZ', 'MSFT', 'INTC', 'MMM'], kind='line')
-
+normal_df_plot = normal_df.plot(use_index=True, 
+                                y=['GE', 'VZ', 'MSFT', 'INTC', 'MMM'], kind='line', 
+                                title='Normal Regression Returns - Our Dataframe', 
+                                xlabel='Periods', 
+                                ylabel='Difference in Returns')
 
 #  Calculations --> LASSO Regression
 GE = lasso_df['GE'].sum().round(4)
@@ -179,8 +182,11 @@ print('')
 print('Total Sum Returns:  ', lasso_df_returns)
 
 # Plot of Returns for Lasso DF
-lasso_df.plot(use_index=True, y=['GE', 'AABA', 'GOOGL', 'KO', 'WMT'], kind='line')
-
+lasso_df_plot = lasso_df.plot(use_index=True, 
+                              y=['GE', 'AABA', 'GOOGL', 'KO', 'WMT'], kind='line', 
+                              title='LASSO Regression Returns - Our Dataframe', 
+                              xlabel='Periods', 
+                              ylabel='Difference in Returns')
 
 # Calculations and Returns --> Ridge Regression
 GE = ridge_df['GE'].sum().round(4)
@@ -201,10 +207,13 @@ print('')
 print('Total Sum Returns:  ', ridge_df_returns)
 
 # Plot of Returns for Ridge DF
-ridge_df.plot(use_index=True, y=['GOOGL', 'AABA', 'GE', 'BA', 'CAT'], kind='line')
+ridge_df_plot = ridge_df.plot(use_index=True, 
+                              y=['GOOGL', 'AABA', 'GE', 'BA', 'CAT'], kind='line', 
+                              title='Ridge Regression Returns - Our Dataframe', 
+                              xlabel='Periods', 
+                              ylabel='Difference in Returns')
 
-
-
+# Regression Calculation Source Dataframe
 ## Regression Calculations --> Normal Returns
 NURO = normal_returns['NURO'].sum().round(4)
 NFLX = normal_returns['NFLX'].sum().round(4)
@@ -225,7 +234,12 @@ print("")
 print('Total Returns Test:', normal_returns_test)
 
 # Plot of Returns for Normal Returns
-normal_returns.plot(use_index=True, y=['NURO', 'NFLX', 'SCON', 'RBCN', 'HHS'], kind='line')
+normal_returns_plot = normal_returns.plot(use_index=True, 
+                                          y=['NURO', 'NFLX', 'SCON', 'RBCN', 'HHS'], 
+                                          kind='line', 
+                                          title='Normal Regression Returns - Source Dataframe', 
+                                          xlabel='Periods', 
+                                          ylabel='Difference in Returns')
 
 
 ## Returns based on their datasets --> LASSO Regression
@@ -247,7 +261,11 @@ print('')
 print('Total Lasso Returns:', lasso_returns_test)
 
 # Plot of Returns for Lasso Returns
-lasso_returns.plot(use_index=True, y=['RGCO', 'FCAP', 'DLTR', 'HIFS', 'EXPO'], kind='line')
+lasso_returns_plot = lasso_returns.plot(use_index=True, 
+                                        y=['RGCO', 'FCAP', 'DLTR', 'HIFS', 'EXPO'], 
+                                        kind='line', 
+                                        title='LASSO Regression Returns - Source Dataframe',
+                                        xlabel='Periods', ylabel='Difference in Returns')
 
 
 ## Return Calculations from their DF --> Ridge Regression
@@ -270,14 +288,22 @@ print("")
 print('Total Returns Test:', ridge_returns_test)
 
 # Plot of Returns for Ridge Returns
-ridge_returns.plot(use_index=True, y=['NURO', 'NFLX', 'SCON', 'RBCN', 'HHS'], kind='line')
+ridge_returns_plot = ridge_returns.plot(use_index=True, 
+                                        y=['NURO', 'NFLX', 'SCON', 'RBCN', 'HHS'], 
+                                        kind='line',
+                                        title = 'Ridge Regression Returns - Source Dataframe', 
+                                        xlabel='Periods',
+                                        ylabel='Difference in Returns')
 
 ## Comparison of Plots to IBB index
 # Comparison to IBB Index
 ibb = ibb.drop(['PX_LAST','% Change'], axis=1)
 
 # Plot showing the ibb indexes Change
-ibb.plot(use_index=True, y=['Change'], kind='line')
+ibb_plot = ibb.plot(use_index=True, y=['Change'], kind='line', 
+                    title='Change of IBB Index Returns over Time', 
+                    xlabel='Periods', 
+                    ylabel='Difference in Returns')
 print('Total IBB Index Returns: ', ibb['Change'].sum().round(4))
 
 
