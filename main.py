0784badf1b6106f67fa63f91a30a3da641a7bfdd
Quintencrum <quintencrum@gmail.com>
2023-05-02 14:04:36 -0600

    updating main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 
 from data import dataImport, visualize
-from models import networkGraphs_dataPrep, networkGraphs_plot
+from models import networkGraphs_dataPrep, networkGraphs_plot, networkGraphsFull
 import numpy as np
 
 
@@ -22,23 +22,42 @@ import numpy as np
 
 def main():
     print('Hello World!')
-    allStock_df_list = networkGraphs_dataPrep.networkDataPrep(dataImport.data_import_ng())
+    # allStock_df_list = networkGraphs_dataPrep.networkDataPrep(dataImport.data_import_ng())
 
-    close_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[0])
-    # open_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[1])
-    # open_close_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[2])
-    # high_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[3])
-    # low_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[4])
+    # close_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[0])
+    # # open_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[1])
+    # # open_close_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[2])
+    # # high_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[3])
+    # # low_dist_matrix = networkGraphs_dataPrep.df_distance_correlation(allStock_df_list[4])
 
-    corrNet_close = networkGraphs_dataPrep.build_corr(close_dist_matrix)
-    # corrNet_open = networkGraphs_dataPrep.build_corr(open_dist_matrix)
-    # corrNet_close_diff = networkGraphs_dataPrep.build_corr(open_close_dist_matrix)
-    # corrNet_high = networkGraphs_dataPrep.build_corr(high_dist_matrix)
-    # corrNet_low = networkGraphs_dataPrep.build_corr(low_dist_matrix)
+    # corrNet_close = networkGraphs_dataPrep.build_corr(close_dist_matrix)
+    # # corrNet_open = networkGraphs_dataPrep.build_corr(open_dist_matrix)
+    # # corrNet_close_diff = networkGraphs_dataPrep.build_corr(open_close_dist_matrix)
+    # # corrNet_high = networkGraphs_dataPrep.build_corr(high_dist_matrix)
+    # # corrNet_low = networkGraphs_dataPrep.build_corr(low_dist_matrix)
 
+    # networkGraphs_plot.plot_network(corrNet_close,'Hello')
+
+    
+    #all stocks data
+    allStock_df_list = networkGraphsFull.networkDataPrep(dataImport.data_import_ng())
+
+    #distance matrices
+    close_dist_matrix = networkGraphsFull.df_distance_correlation(allStock_df_list[0])
+    open_dist_matrix = networkGraphsFull.df_distance_correlation(allStock_df_list[1])
+    open_close_dist_matrix = networkGraphsFull.df_distance_correlation(allStock_df_list[2])
+    high_dist_matrix = networkGraphsFull.df_distance_correlation(allStock_df_list[3])
+    low_dist_matrix = networkGraphsFull.df_distance_correlation(allStock_df_list[4])
+
+    #correlationNetworks
+    corrNet_close = networkGraphsFull.build_corr(close_dist_matrix)
+    corrNet_open = networkGraphsFull.build_corr(open_dist_matrix)
+    corrNet_close_diff = networkGraphsFull.build_corr(open_close_dist_matrix)
+    corrNet_high = networkGraphsFull.build_corr(high_dist_matrix)
+    corrNet_low = networkGraphsFull.build_corr(low_dist_matrix)
+
+    #plotting the various network graphs
     networkGraphs_plot.plot_network(corrNet_close,'Hello')
-
-
 
 
 
